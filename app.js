a0b4cc37b7a5882426129aeeb59a90381f8e9a92
Rajeev Kannav Sharma <rajeev@headerlabs.com>
2015-06-20 15:31:33 +0530

    The Posts Page Completed.

--- app.js
+++ app.js
@@ -1,41 +1,100 @@
-angular.module('flapperNews', [])
+angular.module('flapperNews', ['ui.router'])
+
+    .config([
+        '$stateProvider',
+        '$urlRouterProvider',
+        function ($stateProvider, $urlRouterProvider) {
+
+            $stateProvider
+                .state('home', {
+                    url: '/home',
+                    templateUrl: '/home.html',
+                    controller: 'MainCtrl'
+                })
+
+                .state('posts', {
+                    url: '/posts/{id}',
+                    templateUrl: '/posts.html',
+                    controller: 'PostsCtrl'
+                })
+
+            $urlRouterProvider.otherwise('home')
+
+        }
+    ])
 
     .controller('MainCtrl', [
         '$scope',
         'posts',
         function ($scope, posts) {
-            $scope.test = 'Hello world!';
 
             $scope.posts = posts.posts;
 
             $scope.addPost = function () {
+
                 if (!$scope.title || $scope.title === '') {
                     return;
                 }
+
                 $scope.posts.push({
-                    title: $scope.title, link: $scope.link, upvotes: 0
+                    title: $scope.title,
+                    link: $scope.link,
+                    upvotes: 0,
+                    comments: [
+                        {author: 'Joe', body: 'Cool post!', upvotes: 0},
+                        {author: 'Bob', body: 'Great idea but everything is wrong!', upvotes: 0}
+                    ]
                 })
+
                 $scope.title = ''
                 $scope.link = ''
             }
-            $scope.incrementUpvotes = function(post){
+
+            $scope.incrementUpvotes = function (post) {
                 post.upvotes += 1;
             }
+
         }])
 
+    .controller('PostsCtrl', [
+        '$scope',
+        '$stateParams',
+        'posts',
+        function ($scope, $stateParams, posts) {
+
+
+            $scope.post = posts.posts[$stateParams.id];
+
+
+            $scope.addComment = function (post) {
+                if (!$scope.body || $scope.body == '') {
+                    return;
+                }
+                $scope.post.comments.push({
+                    body: $scope.body,
+                    author: 'user',
+                    upvotes: 0
+                })
+                $scope.body = ''
+            }
+
+        }
+    ])
+
+
     //angular.module('flapperNews', [])
-    .factory('posts', [function(){
+    .factory('posts', [function () {
 
         //service body
 
         var o = {
             posts: [
-                {title: 'Post 1', upvotes: 9},
-                {title: 'Post 2', upvotes: 4},
-                {title: 'Post 3', upvotes: 2},
-                {title: 'Post 4', upvotes: 19},
-                {title: 'Post 5', upvotes: 11},
-                {title: 'Post 6', upvotes: 90}
+                {title: 'Post 1', upvotes: 9, comments: []},
+                {title: 'Post 2', upvotes: 4, comments: []},
+                {title: 'Post 3', upvotes: 2, comments: []},
+                {title: 'Post 4', upvotes: 19, comments: []},
+                {title: 'Post 5', upvotes: 11, comments: []},
+                {title: 'Post 6', upvotes: 90, comments: []}
             ]
         };
 
